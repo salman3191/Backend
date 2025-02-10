03de7f1ba5708ceedafc9e9b7dc4647a1f756465
salman <mohammadsalmankhan3191@gmail.com>
2025-02-10 12:33:13 +0530

    adding ejs direc to git hub backend repo

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  if (data) {
+    res.render("instagram.ejs", { data });
+  } else {
+    res.render("error.ejs");
+  }
+});
+// app.get("/hello", (req, res) => {
+//   res.send("hello");
+// });
+// app.get("/rollDice", (req, res) => {
+//   let num = Math.floor(Math.random() * 6) + 1;
+//   res.render("rollDice.ejs", { diceval: num });
+// });
+
+app.listen(port, (req, res) => {
+  console.log(`listening on port ${port}`);
+});
